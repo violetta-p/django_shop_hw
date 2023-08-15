3aa089e8f7a8a46f46bccf0503d0cef0d8278aaf
ViolettaP <violetta.pukhova@mail.ru>
2023-08-15 21:58:07 +0300

    home_page and contacts templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from catalog.views import home_page_controller, contacts_controller
+#from catalog.views import home_page_controller, contacts_controller
+from catalog import views
+import os
 
 urlpatterns = [
-    path('', home_page_controller),
-    path('', contacts_controller)
+    path('', views.home_page_controller, name = "home_page_controller"),
+    path('contacts/', views.contacts_controller, name = "contacts_controller"),
 ]
