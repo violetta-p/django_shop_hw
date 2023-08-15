cc8b5eec95826088b0479e9876cf77fee25f6ecb
ViolettaP <violetta.pukhova@mail.ru>
2023-08-15 22:12:44 +0300

    fixed control

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from catalog import views
 import os
 
 urlpatterns = [
-    path('', views.home_page_controller, name = "home_page_controller"),
-    path('contacts/', views.contacts_controller, name = "contacts_controller"),
+    path('', views.home_page_controller, name="home_page_controller"),
+    path('contacts/', views.contacts_controller, name="contacts_controller"),
 ]
