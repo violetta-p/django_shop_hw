0573f7c206d230951fa7f3425501b0c38abc0cc9
ViolettaP <violetta.pukhova@mail.ru>
2023-08-17 23:09:37 +0300

    united custom_commands for DB

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,11 +1,18 @@
 from django.core.management import BaseCommand
 
 from catalog.models import Category, Product
+from django.core.management.color import no_style
 from django.db import connection
-from django.core.management import call_command
 
 
 class Command(BaseCommand):
+    Category.objects.all().delete()
+    Product.objects.all().delete()
+
+    sequence_sql = connection.ops.sequence_reset_sql(no_style(), [Category, Product])
+    with connection.cursor() as cursor:
+        for sql in sequence_sql:
+            cursor.execute(sql)
 
     def handle(self, *args, **options):
         products_list = [
