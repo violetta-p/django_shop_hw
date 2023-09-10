3976b86b66b9a0a28dd16380c373ee6e08839070
ViolettaP <violetta.pukhova@mail.ru>
2023-09-10 20:53:03 +0300

    new files

---
+++ users/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from users.models import User
+
+class StyleFormMixin(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+
+class UserProfileForm(StyleFormMixin, UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.HiddenInput()
