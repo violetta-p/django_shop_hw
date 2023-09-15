b75bcb361951de3755954835720239178c6ae421
ViolettaP <violetta.pukhova@mail.ru>
2023-09-15 23:57:44 +0300

    added cache

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
+
 from blog.apps import BlogConfig
 from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
@@ -6,7 +8,7 @@ from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('create/', BlogCreateView.as_view(), name='create'),
+    path('create/', never_cache(BlogCreateView.as_view()), name='create'),
     path('', BlogListView.as_view(), name='list'),
     path('view/<int:pk>', BlogDetailView.as_view(), name='view'),
     path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page, never_cache
+
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView, categories, filtered_items
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name="product_list"),
+    path('product_list/', ProductListView.as_view(), name="product_list"),
     path('contacts/', contacts, name="contacts"),
-    path('products/create/', ProductCreateView.as_view(), name='product_create'),
-    path('products/view/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', never_cache(ProductCreateView.as_view()), name='product_create'),
+    path('products/view/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('products/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
-    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete')
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('', categories, name="categories"),
+    path('<int:pk>/items/', filtered_items, name="filtered_items"),
+
 ]
